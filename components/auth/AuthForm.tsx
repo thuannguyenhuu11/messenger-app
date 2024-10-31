3aaab998b2cd40fb64c14a138e04e5e48aec73b6
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-01 03:50:25 +0700

    feat: add button for Auth UI

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -2,6 +2,7 @@
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Input from '@/components/input/Input';
+import Button from '@/components/button/Button';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -51,8 +52,24 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email" label="Email" register={register} errors={errors} />
+          {variant === 'REGISTER' && <Input id="name" label="Name" register={register} errors={errors} />}
+          <Input id="email" label="Email address" type="email" register={register} errors={errors} />
+          <Input id="password" label="Password" type="password" register={register} errors={errors} />
+
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
