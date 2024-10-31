1e53186973afd9f84b8dce5f496e303ecd87ffbc
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-01 04:04:31 +0700

    feat: update Auth UI with login method

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -3,6 +3,8 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Input from '@/components/input/Input';
 import Button from '@/components/button/Button';
+import AuthSocialButton from '@/components/auth/AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -68,6 +70,23 @@ const AuthForm = () => {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
             </div>
+
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">Or continue with</span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>{variant === 'LOGIN' ? 'New to Messenger ?' : 'Already have an account ?'}</div>
+
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
           </div>
         </div>
       </div>
