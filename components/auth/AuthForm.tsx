1244b09e475b38783009910911e183b3489d7a03
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-01 10:41:15 +0700

    feat: setup prisma and mongodb, create prisma schema

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -54,9 +54,25 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          {variant === 'REGISTER' && <Input id="name" label="Name" register={register} errors={errors} />}
-          <Input id="email" label="Email address" type="email" register={register} errors={errors} />
-          <Input id="password" label="Password" type="password" register={register} errors={errors} />
+          {variant === 'REGISTER' && (
+            <Input id="name" label="Name" register={register} errors={errors} disabled={isLoading} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
 
           <div>
             <Button disabled={isLoading} fullWidth type="submit">
